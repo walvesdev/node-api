125e4121ca2a2997d3d2a198a128330c84e33b28
Willian Alves da Silva <willian__bsb@hotmail.com>
2024-06-18 09:39:57 -0300

    configurado build production

--- tests/cidade/cidade-controller.test.ts
+++ tests/cidade/cidade-controller.test.ts
@@ -3,8 +3,49 @@ import request from "supertest";
 
 
 describe("Controller - Cidade", () => {
-  test("POST/ Create", async () => {
+  
+  test("GET/ GetAll", async () => {
     
+    const response = await request(app)
+      .get("/cidade");
+
+    expect(response.statusCode).toBe(200);
+    
+  });
+
+  test("GET/ GetById", async () => {
+
+    const response = await request(app)
+      .get("/cidade/1");
+
+    expect(response.statusCode).toBe(200);
+
+  });
+
+  test("Delete/ Delete", async () => {
+
+    const response = await request(app)
+      .delete("/cidade/1");
+
+    expect(response.statusCode).toBe(204);
+
+  });
+
+  test("PUT/ Update", async () => {
+
+    const response = await request(app)
+      .put("/cidade")
+      .send({
+        "id":1,
+        "nome":"Barretos"
+      });
+
+    expect(response.statusCode).toBe(204);
+
+  });
+
+  test("POST/ Create", async () => {
+
     const response = await request(app)
       .post("/cidade")
       .send({
@@ -13,7 +54,7 @@ describe("Controller - Cidade", () => {
       });
 
     expect(response.statusCode).toBe(201);
-    
+
   });
 
   return;
